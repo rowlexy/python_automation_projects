1a8aebb7ed4cdf56b134326d3a52798d82be84e3
rowlexy <rowlandelegbede@gmail.com>
2025-10-08 07:27:13 +0200

    total expenses and summary added

--- 1_expense_tracker/script.py
+++ 1_expense_tracker/script.py
@@ -17,21 +17,46 @@ def clean_expenses_data(input_file):
         return None
     
 def add_expenses_data(input_file, output_file):
+    df = clean_expenses_data(input_file)
+    if df is None:
+        return False
+    print('Add a new expense\n-----------------\n')
     try:
-        df = clean_expenses_data(input_file)
-        if df is None:
-            return False
-        print('Add a new expense\n-----------------\n')
-        date_time = input('Enter date and time (DD-MM-YYYY HH:MM): ')
-        if not date_time:
-            parsed_date = datetime.now()
-        elif ' ' not in date_time:
-            parsed_date = datetime.strptime(date_time, '%d-%m-%Y').replace(hour=datetime.now().hour, minute=datetime.now().minute) 
+        attempts = 0
+        parsed_date = None
+        while attempts < 3:
+            date_time = input('Enter date and time (DD-MM-YYYY HH:MM): ')
+            try:
+                if not date_time:
+                    parsed_date = datetime.now()
+                elif ' ' not in date_time:
+                    parsed_date = datetime.strptime(date_time, '%d-%m-%Y').replace(hour=datetime.now().hour, minute=datetime.now().minute) 
+                else:
+                    parsed_date = datetime.strptime(date_time, '%d-%m-%Y %H:%M')
+                break
+            except ValueError:
+                attempts += 1
+                print(f"Invalid date format. Please use 'DD-MM-YYYY' or 'DD-MM-YYYY HH:MM'. {attempts} attempts remaining.\n")
         else:
-            parsed_date = datetime.strptime(date_time, '%d-%m-%Y %H:%M')
+            print("Too many invalid attempts. Exiting the loop.")
+            return None        
         category = input('Enter category: ')
         description = input('Enter a short description: ')
-        amount = input('Enter amount spent: ')
+        
+        attempts = 0
+        amount = None
+        while attempts < 3:
+            amount = input('Enter amount spent: ')
+            try:
+                amount = float(amount)
+                break
+            except ValueError:
+                attempt += 1
+                print(f"Invalid amount. Please enter a numeric value (e.g. 125.50). {attempts} attempts remaining.\n")
+        else:
+            print("Too many invalid attempts. Exiting the loop.")
+            return None
+
         # Collecting the users input in a dictionary
         new_expenses = {
             'Date': parsed_date,
@@ -39,12 +64,23 @@ def add_expenses_data(input_file, output_file):
             'Description': description,
             'Amount': float(amount)
         }
+        # Parsing the df['Date] as datetime
+        df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+        
         # Adding the users input to the dataframe
         df = pd.concat([df, pd.DataFrame([new_expenses])], ignore_index=True)
+        
+        # Summarize by category
+        summary = (df.groupby('Category')['Amount'].sum())
+        # Total monthly expenses
+        monthly_expenses = df[df['Date'].dt.month == datetime.now().month]['Amount'].sum().round(2)
         print(f'Expenses added: [{parsed_date}, {category}, {description}, {amount}]')
         df.to_csv(output_file, index=False)
         print(f'Data appended to {output_file}')
         print(f'Total records now: {len(df)}')
+        print('\nSummary by category:')
+        print(summary)
+        print(f'\nTotal monthly expenses {monthly_expenses:.2f}')
         return df
     except ValueError:
         print('Unable to add data to the dataframe')
